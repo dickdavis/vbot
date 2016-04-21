e247ac8f498a64ac70b4d42ae6137981eaa15ee3
Dick Davis <rdavis89@my.apsu.edu>
2016-04-21 02:09:08 -0500

    separated socket handling and message logic

---
+++ vbotmsglogic.rb
@@ -0,0 +1,72 @@
+
+class VbotMsgLogic
+  attr_reader :msg
+
+  def initialize config
+    @msg = msg
+    @nick = config['nick']
+    @ident = config['ident']
+    @gecos = config['gecos']
+    @chan = config['chan']
+  end
+
+  ##
+  # Gives nick to server
+  #
+  def nick_to_server
+    "NICK #{@nick}\r\n"
+  end
+
+  ##
+  # Identifies with server
+  #
+  def ident_with_server
+    "USER #{@ident} * 8 :#{@gecos}\r\n"
+  end
+
+  ##
+  # Joins to channel on IRC server
+  #
+  def join_channel
+    return "JOIN #{@chan}\r\n"
+  end
+
+  ##
+  # Handles ping message from server
+  #
+  def handle_ping token
+    return "PONG #{token}"
+  end
+
+  ##
+  # Handles private message commands
+  #
+  def hear_command data
+      rt = get_nick_rt data[0]
+      command = data[4]
+      if command.upcase == "HELLO"
+        return "PRIVMSG #{rt} Hello, #{rt}.\r\n"
+      end
+  end
+
+  ##
+  # Gets the nick to reply to
+  #
+  def get_nick_rt msg
+    last = 1
+    msg.each_char.with_index do |char, i|
+      last = i-1 if char == '!'
+    end
+    rt = msg.slice(1, last)
+  end
+
+  ##
+  # Performs logic on message to determine response
+  #
+  def discern data
+    return handle_ping data[1] if data[0] == 'PING'
+    return join_channel if data[1] == '376' || data[1] == '422'
+    return hear_command data if data[1] == 'PRIVMSG' && data[3] == ":#{@nick}"
+  end
+end
+
--- verboten.rb
+++ verboten.rb
@@ -3,6 +3,7 @@
 #
 # Copyright 2016 Richard Davis GPL v3
 require 'socket'
+require './vbotmsglogic'
 
 ##
 # This class serves as an IRC bot
@@ -21,6 +22,7 @@ class Verboten
     @ident = config['ident']
     @gecos = config['gecos']
     @chan = config['chan']
+    @msg_logic = VbotMsgLogic.new config
     start_connection
   end
 
@@ -30,39 +32,13 @@ class Verboten
   def send_msg msg
     @socket.puts msg
   end
-
-  ##
-  # Joins to channel on IRC server
-  #
-  def join_channel
-    send_msg "JOIN #{@chan}\r\n"
-  end
-
-  ##
-  # Handles ping message from server
-  #
-  def handle_ping token
-    send_msg "PONG #{token}"
-  end
-
-  ##
-  # Gets the nick to reply to
-  #
-  def get_nick_rt msg
-    last = 1
-    msg.each_char.with_index do |char, i|
-      last = i-1 if char == '!'
-    end
-    rt = msg.slice(1, last)
-  end
-
   ##
   # Starts the connection to the server and provides identification
   #
   def start_connection
     @socket = TCPSocket.open @server, @port
-    send_msg "NICK #{@nick}\r\n"
-    send_msg "USER #{@ident} * 8 :#{@gecos}\r\n"
+    send_msg "#{@msg_logic.nick_to_server}"
+    send_msg "#{@msg_logic.ident_with_server}"
   end
 
   ##
@@ -73,13 +49,12 @@ class Verboten
       until @socket.eof? do
         buffer = @socket.gets
         puts buffer
-        data = buffer.split
-        handle_ping data[1] if data[0] == 'PING'
-        join_channel if data[1] == '376' || data[1] == '422'
-        if data[1] == "PRIVMSG" && data[3] == ":verboten"
-          msg = data[0]
-          rt = get_nick_rt msg
-          send_msg "PRIVMSG #{rt} Hello, world.\r\n"
+        puts '--------------EOM (server)-------------'
+        msg = buffer.split
+        response = @msg_logic.discern msg
+        unless response.nil?
+          send_msg response
+          puts "#{@nick.upcase} => #{response}"
         end
       end
     rescue
